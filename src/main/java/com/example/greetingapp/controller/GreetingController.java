190386a314cb60590c2d90442fe7395cce3083ea
ruramaimuyezwa-alt <ruramai.muyezwa@gmail.com>
2026-05-13 15:40:48 +0200

    Initial commit

---
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package com.example.greetingapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+
+        model.addAttribute("message", "Welcome to Vistula!");
+
+        return "greeting";
+    }
+}
